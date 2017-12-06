76c50cb68f8396333df06f4126149aba73a2ded1
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-06 14:49:42 -0500

    flesh out elevator, hint at simulation loop, take args

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-console.log("let's make some elevators");
-
-const initBuilding = (numFloors, numElevators) => {
-    const elevator = { floor: 0, trips: 0, destinations: [] };
+const initBuilding = (numElevators, numFloors) => {
+    const elevator = {
+        floor: 1,
+        trips: 0,
+        destinations: [],
+        addDestination: (floor) => { console.log(`add ${floor} to your destination list`) },
+        tick: () => { console.log('let people on/off, move, or idle') }
+    };
 
     return {
         topFloor: numFloors,
         elevators: Array(numElevators).fill(elevator),
-        call: (dest) => { console.log(`please send an elevator to ${dest}`) }
+        call: (dest) => { console.log(`please send an elevator to ${dest}`) },
+        tick: () => { console.log('all elevators move 1 floor, let people on/off, or idle') }
     }
 }
+
+const numElevators = parseInt(process.argv[2]);
+const numFloors = parseInt(process.argv[3]);
+console.log(numElevators);
+console.log(numFloors);
