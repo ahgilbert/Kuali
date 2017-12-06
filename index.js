3e28a44e807c6d167e4de7b47dac143757c74fe7
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-06 14:37:05 -0500

    initial thoughts

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("ahg rulz #1");
+console.log("let's make some elevators");
+
+const initBuilding = (numFloors, numElevators) => {
+    const elevator = { floor: 0, trips: 0, destinations: [] };
+
+    return {
+        topFloor: numFloors,
+        elevators: Array(numElevators).fill(elevator),
+        call: (dest) => { console.log(`please send an elevator to ${dest}`) }
+    }
+}
