b1eac548c771037b33f5eb4bc1b0db08b9bc68e0
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-06 15:54:00 -0500

    sketch elevator call logic, fix syntax error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const initBuilding = (numElevators, numFloors) => {
-    const elevator = (ident) => {
+    const initElevator = (ident) => {
         return {
             id: ident,
             currentFloor: 1,
@@ -40,17 +40,33 @@ const initBuilding = (numElevators, numFloors) => {
                 currentFloor += currentDirection;
             }
         };
+    }
 
-        return {
-            topFloor: numFloors,
-            elevators: Array(numElevators).fill(elevator),
-            call: (dest) => { console.log(`please send an elevator to ${dest}`) },
-            tick: () => { console.log('all elevators tick once') }
+    return {
+        topFloor: numFloors,
+        elevators: Array.from(Array(numElevators).keys()).map(initElevator),
+        call: (dest) => {
+            const findElevator = (dest) => {
+                // if an empty elevator is here, use that
+                // if an elevator is going to be here soon, use that
+                // grab the closest elevator
+            }
+            if (dest > topFloor || dest < 1) {
+                console.log("rejecting out-of-range call. Contact Wonkavator, INC, at 866-11-CANDY");
+                return;
+            }
+            const target = findElevator(dest);
+            target.call(dest);
+        },
+        tick: () => {
+            elevators.forEach(e => e.tick());
         }
-    };
+    }
 };
 
 const numElevators = parseInt(process.argv[2]);
 const numFloors = parseInt(process.argv[3]);
 console.log(numElevators);
 console.log(numFloors);
+const building = initBuilding(numElevators, numFloors);
+building.elevators.map(e => console.log(e.id));
